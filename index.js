e74589544ea275d8e7589e5e1dfd4f12580f018f
laynaria <pro.laynaria@outlook.fr>
2024-05-27 15:23:10 +0200

    Correction of Heracles First Labour for my students

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const { Fighter } = require("./src/Fighter.js");
+
+const [heracles, nemean] = [
+  new Fighter("🧔 Heracles", 20, 6),
+  new Fighter("🦁 Nemean Lion", 11, 13),
+];
+
+let round = 1;
+
+while (heracles.life > 0 && nemean.life > 0) {
+  heracles.fight(nemean);
+  nemean.fight(heracles);
+
+  console.log(`⏲ Round ${round}`);
+  round++;
+
+  console.log(`${heracles.name} ⚔ ${nemean.name} ❤ ${nemean.life}`);
+  console.log(`${nemean.name} ⚔ ${heracles.name} ❤ ${heracles.life}`);
+
+  if (nemean.isAlive()) {
+    nemean.isDead(heracles);
+  } else if (heracles.isAlive()) {
+    heracles.isDead(nemean);
+  }
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,34 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  fight(Ennemy) {
+    const dmg = Math.floor(Math.random() * (this.strength - 1) + 1) + 1;
+
+    const totalDmg = dmg - Ennemy.dexterity;
+
+    Ennemy.life -= totalDmg > 0 ? totalDmg : 0;
+
+    if (Ennemy.life < 0) {
+      Ennemy.life = 0;
+    }
+  }
+
+  isAlive() {
+    return this.life === 0;
+  }
+
+  isDead(Ennemy) {
+    console.log(`${Ennemy.name} 🏆 wins`);
+    console.log(`${this.name} 💀 lost`);
+  }
+}
+
+module.exports = { Fighter };
